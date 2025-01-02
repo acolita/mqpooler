01632d2a955d07c248969e01fc69c76cd42e2089
olavo.marini <olavo.marini@acolita.com.br>
2025-01-02 10:56:56 -0300

    Refatoração em MQOperationsImpl

--- src/main/java/br/com/acolita/mqpooler/MQOperationsImpl.java
+++ src/main/java/br/com/acolita/mqpooler/MQOperationsImpl.java
@@ -9,7 +9,6 @@ import org.slf4j.LoggerFactory;
 
 import java.time.Duration;
 
-
 class MQOperationsImpl implements MQOperations {
     private static final Logger logger = LoggerFactory.getLogger(MQOperationsImpl.class);
     private static final Duration DEFAULT_TIMEOUT = Duration.ofSeconds(30);
@@ -32,45 +31,53 @@ class MQOperationsImpl implements MQOperations {
         MQConnectionTriple triple = null;
         try {
             triple = connectionManager.borrowConnection(queueDef);
-
-            MQMessage sendMessage = new MQMessage();
-            sendMessage.format = "MQSTR   ";
-            sendMessage.characterSet = 37;
-            sendMessage.expiry = (int)timeout.getSeconds();
-            sendMessage.correlationId = "AMQ!NEW_SESSION_CORRELID".getBytes();
-            sendMessage.messageType = 1;
-            sendMessage.replyToQueueName = queueDef.getResponseQueue();
-            sendMessage.writeString(message);
-
-            try {
-                triple.getRequestQueue().put(sendMessage);
-            } catch (MQException e) {
-                connectionManager.invalidateConnection(queueDef, triple);
-                throw e;
-            }
-
-            MQMessage replyMessage = new MQMessage();
-            replyMessage.correlationId = sendMessage.messageId;
-
-            MQGetMessageOptions gmo = new MQGetMessageOptions();
-            gmo.options = CMQC.MQGMO_WAIT;
-            gmo.waitInterval = (int)timeout.toMillis();
-
-            try {
-                triple.getResponseQueue().get(replyMessage, gmo);
-                String response = replyMessage.readStringOfCharLength(replyMessage.getMessageLength());
-                connectionManager.returnConnection(queueDef, triple);
-                return response;
-            } catch (MQException e) {
-                connectionManager.invalidateConnection(queueDef, triple);
-                throw e;
-            }
-
+            MQMessage sendMessage = prepareMessage(message, timeout);
+            sendRequest(triple, sendMessage);
+            String response = receiveResponse(triple, sendMessage, timeout);
+            connectionManager.returnConnection(queueDef, triple);
+            return response;
         } catch (Exception e) {
             if (triple != null) {
                 connectionManager.invalidateConnection(queueDef, triple);
             }
             logger.error("Error in requestResponse operation", e);
+            throw e;
+        }
+    }
+
+    private MQMessage prepareMessage(String message, Duration timeout) throws Exception {
+        MQMessage sendMessage = new MQMessage();
+        sendMessage.format = "MQSTR   ";
+        sendMessage.characterSet = 37;
+        sendMessage.expiry = (int) timeout.getSeconds();
+        sendMessage.correlationId = "AMQ!NEW_SESSION_CORRELID".getBytes();
+        sendMessage.messageType = 1;
+        sendMessage.replyToQueueName = queueDef.getResponseQueue();
+        sendMessage.writeString(message);
+        return sendMessage;
+    }
+
+    private void sendRequest(MQConnectionTriple triple, MQMessage sendMessage) throws MQException {
+        try {
+            triple.getRequestQueue().put(sendMessage);
+        } catch (MQException e) {
+            connectionManager.invalidateConnection(queueDef, triple);
+            throw e;
+        }
+    }
+
+    private String receiveResponse(MQConnectionTriple triple, MQMessage sendMessage, Duration timeout) throws Exception {
+        MQMessage replyMessage = new MQMessage();
+        replyMessage.correlationId = sendMessage.messageId;
+        MQGetMessageOptions gmo = new MQGetMessageOptions();
+        gmo.options = CMQC.MQGMO_WAIT;
+        gmo.waitInterval = (int)timeout.toMillis();
+
+        try {
+            triple.getResponseQueue().get(replyMessage, gmo);
+            return replyMessage.readStringOfCharLength(replyMessage.getMessageLength());
+        } catch (MQException e) {
+            connectionManager.invalidateConnection(queueDef, triple);
             throw e;
         }
     }
