c2ef5fa566c45ea87d9d2cfdf66e715427692631
olavo.marini <olavo.marini@acolita.com.br>
2025-01-02 09:47:23 -0300

    JavaDoc inicial

--- src/main/java/br/com/acolita/mqpooler/IBMRequestResponseService.java
+++ src/main/java/br/com/acolita/mqpooler/IBMRequestResponseService.java
@@ -3,15 +3,35 @@ package br.com.acolita.mqpooler;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+/**
+ * Service for handling request-response operations with IBM MQ.
+ * It manages connections and provides access to {@link MQOperations}
+ * for specified request and response queues.
+ */
 @Service
 public class IBMRequestResponseService {
     private final MQConnectionManager connectionManager;
 
+    /**
+     * Constructs an IBMRequestResponseService with a specified
+     * MQConnectionManager to manage connections.
+     *
+     * @param connectionManager The connection manager responsible for creating
+     *                          and managing MQ connections.
+     */
     @Autowired
     public IBMRequestResponseService(MQConnectionManager connectionManager) {
         this.connectionManager = connectionManager;
     }
 
+    /**
+     * Provides an {@link MQOperations} instance for interacting with the
+     * specified request and response queues.
+     *
+     * @param requestQueue  The name of the request queue.
+     * @param responseQueue The name of the response queue.
+     * @return An instance of {@link MQOperations} for the specified queues.
+     */
     public MQOperations forQueues(String requestQueue, String responseQueue) {
         return connectionManager.forQueues(QueueDefinition.of(requestQueue, responseQueue));
     }
--- src/main/java/br/com/acolita/mqpooler/MQOperations.java
+++ src/main/java/br/com/acolita/mqpooler/MQOperations.java
@@ -2,7 +2,33 @@ package br.com.acolita.mqpooler;
 
 import java.time.Duration;
 
+/**
+ * Provides operations for sending and receiving messages using IBM MQ.
+ * This interface defines the contract for synchronous request-response
+ * message exchanges.
+ */
 public interface MQOperations {
+
+    /**
+     * Sends a request message to a specified queue and waits for a response.
+     * This method uses a default timeout for waiting for the response.
+     *
+     * @param message The message to be sent.
+     * @return The response message received from the queue.
+     * @throws Exception If an error occurs during sending or receiving the message,
+     *                   including any MQ-related issues.
+     */
     String requestResponse(String message) throws Exception;
+
+    /**
+     * Sends a request message to a specified queue and waits for a response
+     * with a specified timeout.
+     *
+     * @param message The message to be sent.
+     * @param timeout The maximum duration to wait for a response.
+     * @return The response message received from the queue.
+     * @throws Exception If an error occurs during sending or receiving the message,
+     *                   including any MQ-related issues, or if the timeout expires.
+     */
     String requestResponse(String message, Duration timeout) throws Exception;
 }
